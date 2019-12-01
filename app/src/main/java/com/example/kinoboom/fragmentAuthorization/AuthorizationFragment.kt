7ec784084fced1c30e810dcfb7a40bb193be95a8
SashaMizernuy <sasha.mizernuy@gmail.com>
2019-12-02 01:16:01 +0300

    Creating a AuthorizationFragment.kt

--- app/src/main/java/com/example/kinoboom/fragmentAuthorization/AuthorizationFragment.kt
+++ app/src/main/java/com/example/kinoboom/fragmentAuthorization/AuthorizationFragment.kt
@@ -14,7 +14,6 @@ class AuthorizationFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        
         return inflater.inflate(R.layout.fragment_authorization, container, false)
     }
 }
